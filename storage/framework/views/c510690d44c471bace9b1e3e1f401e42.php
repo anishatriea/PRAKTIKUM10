800e2e2116411ebaf5dc5303606fc7ab7221e2b8
anishatriea <41880068+anishatriea@users.noreply.github.com>
2023-07-12 19:48:45 +0700

    Add files via upload

---
+++ storage/framework/views/c510690d44c471bace9b1e3e1f401e42.php
@@ -0,0 +1,103 @@
+<?php
+    $currentRouteName = Route::currentRouteName();
+    $pageTitle = 'Login';
+?>
+
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e($pageTitle); ?></title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/sass/app.scss', 'resources/js/app.js']); ?>
+</head>
+<body>
+<style>
+    body {
+        background-color: rgb(49, 108, 244);
+    }
+</style>
+<div class="container-sm my-5 py-5">
+    <form action="<?php echo e(route('login')); ?>" method="POST">
+        <?php echo csrf_field(); ?>
+        <div class="row justify-content-center">
+            <div class="p-5 bg-light rounded-3 border col-xl-4">
+                <div class="mb-5 text-center">
+                    <i class="bi-hexagon-fill m-2" style="font-size: 50px; color: rgb(49, 108, 244);"></i>
+                    <h3>Employee Data Master</h3>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-15 mb-4">
+                        <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus placeholder="Enter Your Email">
+                        <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <span class="invalid-feedback" role="alert">
+                                <strong><?php echo e($message); ?></strong>
+                            </span>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <div class="col-md-15 mb-2">
+                        <input id="password" type="password" class="form-control <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="password" required autocomplete="current-password" placeholder="Enter Your Password">
+                        <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <span class="invalid-feedback" role="alert">
+                                <strong><?php echo e($message); ?></strong>
+                            </span>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row my-3 pt-4">
+                    <div class="col-md-15">
+                        <button type="submit" class="btn btn-primary btn-lg px-0 w-100">
+                            <i class="bi-box-arrow-in-right"></i>
+                            <?php echo e(__('Log In')); ?>
+
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
+<?php /**PATH C:\Users\LENOVO\Desktop\kelompok_8_modul6-master\resources\views/auth/login.blade.php ENDPATH**/ ?>
